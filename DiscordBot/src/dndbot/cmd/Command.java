a359a09e157653f3521b60b6d35b849a8f82be28
Vemahk <Vemahk20@gmail.com>
2018-09-06 11:23:29 -0500

    Moved DND Bot to the jda-repo.

--- DiscordBot/src/dndbot/cmd/Command.java
+++
@@ -1,7 +0,0 @@
-package dndbot.cmd;
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-
-public interface Command {
-	public void run(String[] args, MessageReceivedEvent event);
-	public boolean hasPermissions(MessageReceivedEvent event);
-}
